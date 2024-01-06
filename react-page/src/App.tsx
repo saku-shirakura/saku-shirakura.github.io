1deff18a9d0b59bb35695135f4ffb8de8d92c61a
saku-shirakura <67860312+saku-shirakura@users.noreply.github.com>
2024-01-07 03:09:32 +0900

    お試しReact ライセンス表示は後でやる。

---
+++ react-page/src/App.tsx
@@ -0,0 +1,9 @@
+import my_icon from "./my_icon.svg"
+export const App = () => {
+    return (
+        <>
+            <img src={my_icon} height="128" width="128" alt="user-icon"/>
+            <span>Saku Shirakura</span>
+        </>
+    );
+}
